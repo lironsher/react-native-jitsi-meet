37f633fb71297d8224d42e3cb56107df68c78203
Lirons <liron.sher@gmail.com>
2020-04-09 10:36:41 +0300

    add animitable

--- index.android.js
+++ index.android.js
@@ -3,19 +3,17 @@
  */
 
 import { NativeModules, requireNativeComponent } from 'react-native';
-
+import * as Animatable from 'react-native-animatable';
 export const JitsiMeetView = requireNativeComponent('RNJitsiMeetView');
-export const JitsiMeetModule = NativeModules.RNJitsiMeetModule
+export const JitsiMeetModule = NativeModules.RNJitsiMeetModule;
 const call = JitsiMeetModule.call;
 const audioCall = JitsiMeetModule.audioCall;
 JitsiMeetModule.call = (url, userInfo) => {
-  userInfo = userInfo || {};
-  call(url, userInfo);
-}
+	userInfo = userInfo || {};
+	call(url, userInfo);
+};
 JitsiMeetModule.audioCall = (url, userInfo) => {
-  userInfo = userInfo || {};
-  audioCall(url, userInfo);
-}
+	userInfo = userInfo || {};
+	audioCall(url, userInfo);
+};
 export default JitsiMeetModule;
-
-
